81775bbcdcf3f0d3894c43e4e457ccfcf898ab44
KimJye <eoeogudgud@naver.com>
2018-05-17 20:10:03 +0900

    게시글 번호에 해당되는 게시글을 데이터베이스에서 찾아 게시글의 상세 정보를 보여준다. command=board_view

---
+++ 20180516JSP/src/board_MVC/BoardViewAction.java
@@ -0,0 +1,29 @@
+package board_MVC;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BoardViewAction implements Action{
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String url="/20180516JSP/boardView.jsp";
+
+		String num = request.getParameter("num");
+
+		BoardDAO bDao = BoardDAO.getInstance();
+
+		bDao.updateReadCount(num);
+
+		BoardVO bvo = bDao.selectOneBoardByNum(num);
+
+		request.setAttribute("board", bvo);
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		dispatcher.forward(request,response);
+	}
+
+}
