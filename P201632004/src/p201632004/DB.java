67bab54713d46fc5f77a6f39e015529fb6d4a135
KimJye <eoeogudgud@naver.com>
2018-05-30 13:19:50 +0900

    JSP 쪽지시험 - DB연결

---
+++ P201632004/src/p201632004/DB.java
@@ -0,0 +1,19 @@
+package p201632004;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DB {
+	static DataSource dataSource = null; //context.xml에 적은 DB연결정보
+	public static Connection getConnection(String databaseName) throws SQLException, NamingException {//Connection 객체를 얻는다.
+		   if (dataSource == null) {
+			   InitialContext context = new InitialContext();
+			   dataSource = (DataSource)context.lookup("java:comp/env/jdbc/" + databaseName);
+		   }
+		   return dataSource.getConnection();
+	}
+}
